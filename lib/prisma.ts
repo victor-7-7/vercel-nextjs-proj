97c16c61cedfa1616b55630d2f68c46784d4639a
victor-7-7 <Victor.7.7@ya.ru>
2023-02-16 14:05:44 +0700

    Add comments

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,6 +6,8 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
     prisma = new PrismaClient();
 } else {
+    // https://blog.logrocket.com/what-is-globalthis-why-use-it/
+    // https://github.com/tc39/proposal-global
     if (!global.prisma) {
         global.prisma = new PrismaClient();
     }
--- pages/create.tsx
+++ pages/create.tsx
@@ -4,24 +4,33 @@ import Layout from '../components/Layout';
 import Router from 'next/router';
 
 const Draft: React.FC = () => {
+  // title and content properties are extracted from the component state
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
 
+  // When form submitted, the submitData function is called.
+  // In that function, you need to pass the data from the React
+  // component to an API route which can then handle the actual
+  // storage of the new post data in the database.
   const submitData = async (e: React.SyntheticEvent) => {
     e.preventDefault();
     try {
       const body = { title, content };
+      // submit body via an HTTP POST request to the api/post API route.
       await fetch('/api/post', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(body),
       });
+      // Afterwards, you're redirecting the user to the /drafts page
+      // so that they can immediately see their newly created draft.
       await Router.push('/drafts');
     } catch (error) {
       console.error(error);
     }
   };
 
+  // It renders a form with several input fields.
   return (
     <Layout>
       <div>
